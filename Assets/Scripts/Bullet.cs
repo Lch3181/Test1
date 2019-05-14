4b7bf445848a1efba5acbc661ea62130aab8cd3b
Mitchell7583 <lamchniho@gmail.com>
2019-05-14 15:38:32 -0400

    Fire point on gun keep dropping Y-axis for no reason
    Stuck

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float Damage;
+    public float Speed;
+    public float maxDist;
+    private float lifeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+        lifeTime += 1 * Time.deltaTime;
+
+        if (lifeTime >= maxDist)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public ParticleSystem ShootEffect;
+    public GameObject Bullet;
+    public GameObject FirePoint;
+    public float FireRate;
+
+    private float delay = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (ShootEffect = GetComponent<ParticleSystem>())
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Fire point position: "+FirePoint.transform.position);
+
+        delay -= Time.deltaTime;
+
+        if(Input.GetButton("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    //Fire bullet
+    void Shoot()
+    {
+        if (delay <= 0)
+        {
+            Instantiate(Bullet, FirePoint.transform.position, FirePoint.transform.rotation);
+            delay = FireRate;
+        }
+    }
+}
